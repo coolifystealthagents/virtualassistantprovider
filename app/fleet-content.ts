196d1249c471ab57a1d4337aca57f3cf5947685f
VirtualAssistantProvider automation <automation@virtualassistantprovider.com>
2026-09-07 13:54:22 +0000

    content: publish September 7 blog and research batch

--- app/fleet-content.ts
+++ app/fleet-content.ts
@@ -6,7 +6,9 @@ import { august31ResearchPosts } from './article-research-aug31-2026';
 import { september1ResearchPosts } from './article-research-sep1-2026';
 import { september2ResearchPosts } from './article-research-sep2-2026';
 import { september3ResearchPosts } from './article-research-sep3-2026';
+
 import { september4ResearchPosts } from './article-research-sep4-2026';
+import { september7ResearchPosts } from './article-research-sep7-2026';
 import { august23ResearchPosts } from './article-research-aug23-2026';
 
 export const fleetServices = [
@@ -812,6 +814,7 @@ researchPosts = [...researchPosts, ...publishingResearchConfigs.map(makePublishi
   .concat(august31ResearchPosts)
   .concat(september2ResearchPosts)
   .concat(september4ResearchPosts)
+  .concat(september7ResearchPosts)
   .concat(september3ResearchPosts)
   .concat(september1ResearchPosts)
   .sort((a, b) => b.published.localeCompare(a.published));
